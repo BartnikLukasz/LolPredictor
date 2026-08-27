b29c713412519dbb89b3897bc82a4813c32a5967
BartnikLukasz <bartnik.luk@gmail.com>
2026-08-27 17:19:07 +0200

    Implemented firt_pick - 65,91 Accuracy

--- main.py
+++ main.py
@@ -4,26 +4,26 @@ from player_stats_calculator import compute_player_and_mastery_stats
 from model_trainer import train_lol_prediction_model
 
 if __name__ == '__main__':
-    # prepare_oracles_elixir_pregame(["dataset/match/2014_match_data.csv",
-    #                                 "dataset/match/2015_match_data.csv",
-    #                                 "dataset/match/2016_match_data.csv",
-    #                                 "dataset/match/2017_match_data.csv",
-    #                                 "dataset/match/2018_match_data.csv",
-    #                                 "dataset/match/2019_match_data.csv",
-    #                                 "dataset/match/2020_match_data.csv",
-    #                                 "dataset/match/2021_match_data.csv",
-    #                                 "dataset/match/2022_match_data.csv",
-    #                                 "dataset/match/2023_match_data.csv",
-    #                                 "dataset/match/2024_match_data.csv",
-    #                                 "dataset/match/2025_match_data.csv",
-    #                                 "dataset/match/2026_match_data.csv"],
-    #                                "dataset/pregame/pregame.csv")
+    prepare_oracles_elixir_pregame(["dataset/match/2014_match_data.csv",
+                                    "dataset/match/2015_match_data.csv",
+                                    "dataset/match/2016_match_data.csv",
+                                    "dataset/match/2017_match_data.csv",
+                                    "dataset/match/2018_match_data.csv",
+                                    "dataset/match/2019_match_data.csv",
+                                    "dataset/match/2020_match_data.csv",
+                                    "dataset/match/2021_match_data.csv",
+                                    "dataset/match/2022_match_data.csv",
+                                    "dataset/match/2023_match_data.csv",
+                                    "dataset/match/2024_match_data.csv",
+                                    "dataset/match/2025_match_data.csv",
+                                    "dataset/match/2026_match_data.csv"],
+                                   "dataset/pregame/pregame.csv")
 
-    enriched_df, team_leaderboard = compute_team_elo_ratings(
+    compute_team_elo_ratings(
         filepath="dataset/pregame/pregame.csv",
         output_filepath="dataset/pregame/pregame_dataset_with_elo.csv",
         init_rating=1500,
-        blue_side_bonus=10.0,
+        first_pick_bonus=10.0,
         season_soft_reset_factor=0.2
     )
 
